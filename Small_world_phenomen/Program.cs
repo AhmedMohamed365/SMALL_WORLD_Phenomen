4b49a0bb5c99cc9df060074fdfb7e7c25189c649
AhmedMohamed365 <ahmedmfreelance@gmail.com>
2022-04-23 16:06:26 +0200

    Add project files.

---
+++ Small_world_phenomen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Small_world_phenomen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
