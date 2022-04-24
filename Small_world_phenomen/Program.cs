d4c7802d2c929ac13e8332c3a5b054af71ca30cf
AhmedMohamed365 <ahmedmfreelance@gmail.com>
2022-04-24 04:35:16 +0200

    enhancing reading queries in large files

--- Small_world_phenomen/Program.cs
+++ Small_world_phenomen/Program.cs
@@ -10,14 +10,24 @@ namespace Small_world_phenomen
     {
         static void Main(string[] args)
         {
-            const string defualtPath = "Test/Sample/movies1.txt";
-            Dictionary<string, LinkedList<string>> moviesData = TestUnit.readMovies(defualtPath);
+            const string defaultSample = "Test/Sample/";
+            const string defaultComplete = "Test/Complete/small/Case1/";
+            Dictionary<string, LinkedList<string>> moviesData = TestUnit.readMovies(defaultComplete + "movies193.txt");
 
             foreach(var pair in moviesData)
             {
                 Console.WriteLine("key : " + pair.Key + "  value: " + pair.Value.First.Value);
             }
 
+
+            List<KeyValuePair<string, string>> queries = TestUnit.readQueries(defaultComplete + "queries110.txt");
+
+            queries.ForEach(pair =>
+           {
+
+               Console.WriteLine("actor : " + pair.Key + " || Relate to actor: " + pair.Value);
+               
+           });
         }
     }
 }
--- Small_world_phenomen/TestUnit.cs
+++ Small_world_phenomen/TestUnit.cs
@@ -10,16 +10,38 @@ namespace Small_world_phenomen
    static  class TestUnit
     {
 
-        public static  void readQueries(string path)
-        {
 
+        public static List<KeyValuePair<string, string>> readQueries(string path)
+        {
+            List<KeyValuePair<string, string>> actors_pairs = new List<KeyValuePair<string, string>>(10);
+            char[] seprators = { ',','/' };
+            StreamReader reader = new StreamReader(path);
+
+            KeyValuePair<string, string> actor_pair;
+            while (!reader.EndOfStream)
+            {
+
+                string[] values = reader.ReadLine().Split(seprators);
+
+                for(int i = 0; i <values.Length -1;i++)
+                {
+                    actor_pair = new KeyValuePair<string, string>(values[i], values[i+1]);
+
+                    actors_pairs.Add(actor_pair);
+                }
+                
+
+            }
+
+
+            return actors_pairs;
         }
         public static Dictionary<string, LinkedList<string>> readMovies(string path)
         {
             //Movie //num //actor / actor2
 
-            char[] seprators = { ' ', '/' };
-            Dictionary<string, LinkedList<string>> movieData = new Dictionary<string, LinkedList<string>>(10);
+            char[] seprators = { ',', '/' };
+            Dictionary<string, LinkedList<string>> movieData = new Dictionary<string, LinkedList<string>>();
 
             StreamReader reader = new StreamReader(path);
             
@@ -33,9 +55,9 @@ namespace Small_world_phenomen
 
                string [] input =  line.Split(seprators);
 
-                movieData.Add(input[1], actor_list);
+                movieData.Add(input[0] , actor_list);
 
-                for(int i = 2; i <input.Length;i++)
+                for(int i = 1; i <input.Length;i++)
                 {
                     actor_list.AddLast(input[i]);
                 }
